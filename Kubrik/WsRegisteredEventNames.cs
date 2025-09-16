91a22edda021b9c3d2cab24422b326e53d732e55
patrick <patrick.kubik@outlook.com>
2025-09-17 00:19:06 +0200

    refactor: standardize constant naming in WsRegisteredEventNames

--- Kubrik/WsRegisteredEventNames.cs
+++ Kubrik/WsRegisteredEventNames.cs
@@ -2,6 +2,6 @@ namespace Kubrik;
 
 public readonly struct WsRegisteredEventNames
 {
-    public const string BroadcastLocation = "BROADCAST_Location";
-    public const string UpdateLocation = "UPDATE_Location";
+    public const string BroadcastLocation = "BROADCAST_LOCATION";
+    public const string UpdateLocation = "UPDATE_LOCATION";
 }
